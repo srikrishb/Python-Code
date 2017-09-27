e4492bd64078d88b6303f6ae8217c002bcf0bc1c
Srikrishna Bingi <srikrishna.bingi@exusia.com>
2017-09-27 10:17:24 -0400

    09272017 II'

--- metrics/main.py
+++ metrics/main.py
@@ -5,8 +5,7 @@ from CreateDataFile import CreateDataFile
 from Job import Job
 import shutil
 import yaml
-from openpyxl import load_workbook
-import redis
+from ProcessMetrics import ProcessMetrics
 
 if __name__ == '__main__':
 
@@ -71,63 +70,8 @@ if __name__ == '__main__':
                                 #                os.rename(jobMessage['id'], 'File.xlsx')
                                 shutil.copyfile(jobMessage['id'], "K:/Git Code/Python/Output/File.xlsx")
 
-                                # Open the data file
-                                workbook = load_workbook("K:/Git Code/Python/Output/File.xlsx")
-                                defaultSheet = workbook.active
+                                processMetricsObj = ProcessMetrics("K:/Git Code/Python/Output/File.xlsx")
+                                processMetricsObj.generateMetricsFile()
 
-                                # Kick start redis
-                                redis_db = redis.StrictRedis(host="127.0.0.1", port=6379, db=0)
-                                redis_db.flushall()
-                                i=0
-                                key = ''
-                                offset = 2
-                                prev_assettype = ''
-                                curr_assettype = ''
-                                prev_offset = 2
-                                currAssetName = ''
-                                prevAssetName = ''
-                                keyList =[]
-                                for row in defaultSheet.rows:
-                                    if i == 0:
-                                        i+=1
-                                        for cellInRow in row:
-                                            if cellInRow.value == 'AssetType':
-                                                marker = cellInRow.col_idx
-                                                #xy = coordinate_from_string(cell)  # returns ('A',4)
-                                                #marker = column_index_from_string(xy[0])  # returns 1
-                                                print('marker', marker)
-                                    else:
-                                        for cellInRow in row:
-                                            if cellInRow.col_idx == 1:
-                                                currAssetName = cellInRow.value
-                                            if cellInRow.col_idx > 1 and cellInRow.col_idx < marker:
-                                                if cellInRow.col_idx == 2:
-                                                    key = cellInRow.value
-                                                else:
-                                                    key = key + ":" + cellInRow.value
-                                            if cellInRow.col_idx == marker:
-                                                key = key + ":" + cellInRow.value
-                                                curr_assettype = cellInRow.value
-                                                if prev_assettype == curr_assettype:
-                                                    if prevAssetName == currAssetName:
-                                                        offset = prev_offset
-                                                    else:
-                                                        offset += 1
-                                                        prevAssetName = currAssetName
-                                                else:
-                                                    if prevAssetName != currAssetName:
-                                                        offset =2
-                                                        prev_offset = offset
-                                                        prev_assettype = curr_assettype
-                                                        prevAssetName = currAssetName
-                                        print(key, offset-1, 1)
-                                        keyList.append(key)
 
-                                        # see what keys are in Redis
-
-                                        redis_db.setbit(key, offset-1, 1)
-                                        key = ''
-
-                                for key in keyList:
-                                    print(key, redis_db.bitcount(key))
 
